91522ce81653d147343efbc62e7cbbf53e406dfb
Lakshit Jain <lakshit2006jain@gmail.com>
2026-07-09 10:44:07 +0530

    Update db_connect.php

--- 25ESKCX054/Day-10/db_connect.php
+++ 25ESKCX054/Day-10/db_connect.php
@@ -5,7 +5,7 @@
  $password = "12345";
  $database ="industrial_training";
 
- $conn= mysqli_connect($host, $user, $password, $database);
+ $conn= mysqli_connect($host, $username, $password, $database);
 
  if(!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
