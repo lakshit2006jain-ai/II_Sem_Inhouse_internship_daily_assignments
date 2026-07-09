f491ed54466bfac4df65d9af38a560388eb4777f
Lakshit Jain <lakshit2006jain@gmail.com>
2026-07-09 10:40:33 +0530

    Create Login.php

---
+++ 25ESKCX054/Day-10/Login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Login Page</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <div class="container mt-5" style="max-width: 500px;">
+
+    <h2 class="mb-3">Login</h2>
+
+    <form action="checkLogin.php" method="POST">
+
+    <input type="email" class="form-control mb-3" name="email" placeholder="Enter your Email" required>
+
+        <input type="password" class="form-control mb-3" name="password" placeholder="Enter your Password" required>
+
+        <button class="btn btn-primary w-100">Login</button>
+    </form>
+
+    </div>
+</body>
+</html>
